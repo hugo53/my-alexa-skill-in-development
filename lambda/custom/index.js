bda1c3faaf2dc81b22b1689d679105221f46fbee
mizokami <r.mizokami@gmail.com>
2018-02-13 00:22:26 +0900

    Handle with state

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,55 +1,82 @@
 'use strict';
 var Alexa = require("alexa-sdk");
 
-// For detailed tutorial on how to making a Alexa skill,
-// please visit us at http://alexa.design/build
+var APP_ID = process.env.APP_ID;
 
+var languageStrings = {
+  'en-US': {
+    'translation': {
+      'BYE': 'Good bye.',
+      'ASK_BIRTHDAY': 'Please tell me your birthday.',
+      'ASK_LAST_AGE': 'Please tell me how long you want to live.',
+      'ANSWER_REST_OF_YOUR_LIFE': 'Rest of your life is 18,250 days.  It is 438,000 hours.'
+    }
+  },
+  'ja-JP': {
+    'translation': {
+      'BYE': '終了します。',
+      'ASK_BIRTHDAY': 'あなたの誕生日を教えてください。',
+      'ASK_LAST_AGE': 'あなたは何歳まで生きたいですか。',
+      'ANSWER_REST_OF_YOUR_LIFE': 'あなたの残りの人生は18,250日で、時間で表すと438,000時間です。'
+    }
+  }
+};
 
 exports.handler = function(event, context) {
   var alexa = Alexa.handler(event, context);
-  alexa.appId = process.env.APP_ID;
-  alexa.registerHandlers(handlers);
+  alexa.appId = APP_ID;
+  alexa.resources = languageStrings;
+  alexa.registerHandlers(
+    handlers,
+    askingBirthdayHandlers,
+    answeringBirthdayHandlers,
+    askingLastAgeHandlers,
+    answeringLastAgeHandlers
+  );
   alexa.execute();
 };
 
 var handlers = {
   'LaunchRequest': function () {
-    this.emit('AskYourBirthday');
+    this.handler.state = '_ASKING_BIRTHDAY_STATE';
+    this.emitWithState('Unhandled', false);
   },
-
-  'BirthdayIsIntent': function () {
-    // TODO: Save birthday into somewhere in session
-    var birthday = this.event.request.intent.slots.birthday.value;
-
-    this.emit('AskHowLongYouWantToLive');
+  "AMAZON.CancelIntent": function() {
+    this.emit(':tell', this.t("BYE"));
   },
-
-  'LastAgeIsIntent': function () {
-    // TODO: Save lastAge into somewhere in session
-    var lastAge = this.event.request.intent.slots.lastAge.value;
-
-    this.emit('AnserRestOfYourLife');
-  },
-
-  'SessionEndedRequest' : function() {
-    console.log('Session ended with reason: ' + this.event.request.reason);
-  },
-
-  'Unhandled' : function() {
-    this.emit(':tell', "Sorry, I didn't get that.");
-  },
-
-  'AskYourBirthday': function () {
-    this.emit(':ask', 'Please tell me your birthday.');
-  },
-
-  'AskHowLongYouWantToLive': function () {
-    this.emit(':ask', 'Please tell me how long you want to live.');
-  },
-
-  'AnserRestOfYourLife': function () {
-    // TODO: Calculate a rest of your life from birthday and lastAge
-
-    this.emit(':tell', 'Rest of your life is 18,250 days.  It is 438,000 hours.');
+  "AMAZON.StopIntent": function() {
+    this.emit(':tell', this.t("BYE"));
   }
 };
+
+var askingBirthdayHandlers = Alexa.CreateStateHandler('_ASKING_BIRTHDAY_STATE', {
+  'Unhandled': function () {
+    this.handler.state = '_ANSWERING_BIRTHDAY_STATE';
+    this.emit(':ask', this.t("ASK_BIRTHDAY"));
+  }
+});
+
+var answeringBirthdayHandlers = Alexa.CreateStateHandler('_ANSWERING_BIRTHDAY_STATE', {
+  'AnsweredBirthday': function () {
+    var birthday = this.event.request.intent.slots.birthday.value;
+
+    this.handler.state = '_ASKING_LAST_AGE_STATE';
+    this.emitWithState('Unhandled', false);
+  }
+});
+
+var askingLastAgeHandlers = Alexa.CreateStateHandler('_ASKING_LAST_AGE_STATE', {
+  'Unhandled': function () {
+    this.handler.state = '_ANSWERING_LAST_AGE_STATE';
+    this.emit(':ask', this.t("ASK_LAST_AGE"));
+  }
+});
+
+var answeringLastAgeHandlers = Alexa.CreateStateHandler('_ANSWERING_LAST_AGE_STATE', {
+  'AnsweredLastAge': function () {
+    var lastAge = this.event.request.intent.slots.lastAge.value;
+
+    this.handler.state = '';
+    this.emit(':tell', this.t("ANSWER_REST_OF_YOUR_LIFE"));
+  }
+});
